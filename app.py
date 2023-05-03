a8020162d563ab43554dc7a7e93ce6c1c030cde1
Igor Martins <113381179+igormartins0301@users.noreply.github.com>
2023-05-03 09:11:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import seaborn as sns
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import yfinance as yf
 import pyti
 import plotly.express as px
